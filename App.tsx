34bc9d8d1cc5f01132501af9d1594a208221360f
Vignesh-Vijayakumar-07 <55661474+Vignesh-Vijayakumar-07@users.noreply.github.com>
2023-04-06 15:45:31 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,14 @@
-<%@ taglib prefix="s" uri="/struts-tags" %>
-
-<html>
-
-<head> 
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"> 
-    <title>USER MANAGEMENT :: Error Page</title>
-</head>
-
-<body>
-    <s:if test="#request.getAttribute('org.apache.struts.action.ERROR') != null">
-        <s:property value="#request.getAttribute('org.apache.struts.action.ERROR').getMessage()"/>
-    </s:if>
-    
-    <h4>Contact Administrator to Add Your Bank Id</h4>
-    
-    <s:url var="genericAccountLoginUrl" action="genericAccountLogin">
-        <s:param name="redirectUrl">https://mfaig.global.standardchartered.com:443/odsrdm</s:param>
-    </s:url>
-    
-    <a href="<s:property value="#genericAccountLoginUrl"/>">Go to Home page for GenericID User</a>
-</body>
-
-</html>
+<%@ taglib uri="/struts-tags" prefix="s" %>
+<s:if test="%{#request['org.apache.struts.action.ERROR'] != null}">
+    <s:set var="aes" value="%{#request['org.apache.struts.action.ERROR']}" />
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <s:if test="%{#aes['errors'] != null}">
+                <td valign="top" class="ERROR">&nbsp;&nbsp; <s:actionerror /></td>
+            </s:if>
+            <s:if test="%{#aes['info'] != null}">
+                <td valign="top" class="INFO">&nbsp; &nbsp; <s:actionmessage /></td>
+            </s:if>
+        </tr>
+    </table>
+</s:if>
